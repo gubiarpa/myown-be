27ee30ad00fb001c98c91a281e0b482ae97bde78
gubiarpa <billy.arredondo@pucp.pe>
2021-03-14 17:47:58 -0500

    Server class created
    Taking it to a separate class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 require('dotenv').config();
-const express = require('express');
-const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
-});
+const Server = require('./models/server');
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${ process.env.PORT }`);
-});
+const server = new Server();
+server.listen();
